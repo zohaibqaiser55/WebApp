ede74407bade9112edf98fcf3032c08dd9faa970
Zohaib Qaiser <zohaibqaiser55@gmail.com>
2020-04-26 18:17:38 +0100

    in this commit i have created a modle for forum and had updated the  functionality of expension pannel and also added some attributes for the inputs(in post.create.html) and also implemented some basic validation for the forums that we are inputing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './posts/post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'main-api';
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,19 +1,31 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { NgForm } from '@angular/forms';
+
+//importing the Module we created (Post.module)
+import { Post} from "../post.model"
 
 @Component({
+  //paths to the file (components)
   selector: "app-post-create",
+  //template of the post view
   templateUrl: "./post-create.component.html",
+  //styling shit for the above HTML file
   styleUrls: ["./post-create.component.css"]
 })
+
 export class PostCreateComponent {
   enteredTitle = "";
   enteredContent = "";
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+  onAddPost(from: NgForm) {
+    //some basic HTML validations
+    if (from.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: from.value.title,
+      content: from.value.content
     };
     this.postCreated.emit(post);
   }
