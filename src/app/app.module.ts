56d1016ffc21c48370cba728ef8e5516fcd347cb
Zohaib Qaiser <zohaibqaiser55@gmail.com>
2020-04-26 16:07:28 +0100

    here i have added a simple expansion panel below the text field (by creating a seperate component //PostList component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,31 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HeaderComponent} from "./header/header.component"
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
+    //declaring so can use the components that i created 
     AppComponent,
-    PostCreateComponent,HeaderComponent
+    PostCreateComponent,HeaderComponent,PostListComponent
   ],
   imports: [
+    //all the material components that we import for styling the front end
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
