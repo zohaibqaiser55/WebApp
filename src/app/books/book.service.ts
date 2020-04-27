a755c2343c4a5a15294ea230a8fc515dcd1f7f8a
Zohaib Qaiser <zohaibqaiser55@gmail.com>
2020-04-27 03:40:57 +0100

    here i have created a simple a local server and add some data in it (locally) and try to display the data on the interface  (angular interface)

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,29 +1,39 @@
-import{Post} from './post.model';
 import {Injectable} from '@angular/core'
+import { HttpClient } from "@angular/common/http";
 import { Subject} from 'rxjs'
+
+import{Post} from './post.model';
+
 
 //make one instance of post.service at root level that is avaliable for whole
 @Injectable ({providedIn: 'root'})
 export class PostsService{
 //a private array(real array )
  private  posts: Post [] =[];
- //creating subject as a private property // will be used to pass the posts
+ //creating subject as a private property // will be used to pass the posts(book data)
  private postsUpdated = new Subject<Post[]>();
 
- //getting the value of the real array
+  //injecting http client functionality through constructor
+ constructor(private http: HttpClient) {}
+
+ //getting the value from the server
  getPosts(){
-     //creating a new array with old objects
-     //even if i did change the value it will not efect the real array 
-     return [...this.posts];
+  //specifying that which type of data we wil get 
+  this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/book')
+   //passing the argument
+  .subscribe((postData) => {
+  this.posts = postData.posts;
+  this.postsUpdated.next([...this.posts]);
+  });
  }
  
  getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
 
-//method for adding new post
+//method for adding new posts (book data)
  addPost(title: string, content: string) {
-     //new variable of type, Post
+    //new variable of type, Post
     const post: Post = {id: null, title: title, content: content};
     //after posting it will add the new value(post) in the array [...this.posts])
     this.posts.push(post);
